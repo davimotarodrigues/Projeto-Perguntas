ff47b3b7cb854abe5e47a5405431c3fe37ca519a
Davi Mota Rodrigues <motarodri.davi@gmail.com>
2026-05-05 09:43:21 -0300

    Update server.js

--- server.js
+++ server.js
@@ -208,5 +208,5 @@ io.on("connection", (socket) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log("Servidor rodando na porta " + PORT);
+  console.log(`Servidor rodando na porta http://localhost:${PORT}`);
 });
